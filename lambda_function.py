d1db33031ec49c159b5b7034bcf9adff29f145ea
shammichalas <shammichalas0007@gmail.com>
2025-08-27 16:47:22 +0530

    feat: implement basic Lambda handler for CI/CD pipeline testing

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,6 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from GitHub Actions Lambda Deployment!')
+        'body': json.dumps('Hello Babu That the GitHub Actions Lambda Deployment!')
     }
 
